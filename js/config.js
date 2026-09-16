a3dc83f6af1b24115a45883b566a6eb0813292bd
bittu <bhaskarkuntumalla@gmail.com>
2026-09-16 08:28:30 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// ============================================================================
-// Fill these in from your Supabase project:
-// Project Settings -> API -> Project URL / anon public key
-// ============================================================================
-export const SUPABASE_URL = 'https://rumjmzlovtdypfjyfvgk.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1bWptemxvdnRkeXBmanlmdmdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0NTMwMDcsImV4cCI6MjEwNTAyOTAwN30.QKehBFHVagI8u4dOeFpGyHEQNAMPWiDxcDtmyNE6E1I';
